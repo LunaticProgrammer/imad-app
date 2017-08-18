766eb8364b98f79c6557ce98d79cc91a72054573
LunaticProgrammer <30633466+LunaticProgrammer@users.noreply.github.com>
2017-08-18 23:15:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto = require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
@@ -8,6 +9,20 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/hash/:input', function(req, res){
+   var hashedString = hash(req.params.input, 'this-is-a-random-string') ;
+   res.send(hashedString);
+    
+});
+
+function hash(input){
+    
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return ['pbkd2',hashed.toString(hex)];
+}
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
