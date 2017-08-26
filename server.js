d081a2f99a3c3f56ab0a9a43c2eb24d235e3d5a4
LunaticProgrammer <30633466+LunaticProgrammer@users.noreply.github.com>
2017-08-27 00:40:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,22 @@ function hash(input){
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return ['pbkd2',hashed.toString(hex)];
 }
+function main() {
+  $('.skillset').hide();
+  $('.skillset').fadeIn(1000);
+  
+  $('.projects').hide();
+  
+  $('.projects-button').on('click', function() {
+//$(this).next().toggle();
+    $(this).toggleClass('active');
+    $(this).text('Projects Viewed');
+    $(this).next().slideToggle(400);
+	});
+  
+}
 
+$(document).ready(main);
 
 
 app.get('/ui/style.css', function (req, res) {
